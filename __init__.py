97b5e17c9ba8e2bd2f4c90993bf2a31b73f39d9d
Noah Wahl <noah.ares@yahoo.de>
2020-08-22 14:54:30 +0200

    Finish basic funktionality
    * turn light of/on
    * settings ip address

--- __init__.py
+++ __init__.py
@@ -9,12 +9,22 @@ class Yeelight(MycroftSkill):
         MycroftSkill.__init__(self)
 
     def initialize(self):
+        bulb_ip = self.settings.get('bulb_ip', "192.168.0.129")
+        bulb = Bulb(bulb_ip)
+
         yeelight_on_intent = IntentBuilder('OnIntent').require('lightKeyword').require('onKeyword').build()
         self.register_intent(yeelight_on_intent, self.handle_yeelight_on_intent)
+
+        yeelight_off_intent = IntentBuilder('OffIntent').require('lightKeyword').require('offKeyword').build()
+        self.register_intent(yeelight_off_intent, self.handle_yeelight_off_intent)
 
     def handle_yeelight_on_intent(self, message):
         self.speak_dialog('yeelight.on')
         bulb.turn_on()
+
+    def handle_yeelight_off_intent(self, message):
+        self.speak_dialog('yeelight.off')
+        bulb.turn_off()
 
 def stop(self):
     pass
