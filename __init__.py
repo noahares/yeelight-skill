d73b9c383a5d7fbccb54b09b0befaa91a932bf21
Noah Wahl <noah.ares@yahoo.de>
2020-08-22 13:20:17 +0200

    Create initial working setup
    turn on lights

--- __init__.py
+++ __init__.py
@@ -1,15 +1,23 @@
 from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from yeelight import Bulb
 
+bulb = Bulb("192.168.0.129")
 
 class Yeelight(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('yeelight.intent')
-    def handle_yeelight(self, message):
-        self.speak_dialog('yeelight')
+    def initialize(self):
+        yeelight_on_intent = IntentBuilder('OnIntent').require('lightKeyword').require('onKeyword').build()
+        self.register_intent(yeelight_on_intent, self.handle_yeelight_on_intent)
 
+    def handle_yeelight_on_intent(self, message):
+        self.speak_dialog('yeelight.on')
+        bulb.turn_on()
+
+def stop(self):
+    pass
 
 def create_skill():
     return Yeelight()
-
